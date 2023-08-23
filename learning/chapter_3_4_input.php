294afc879cff7f02a5ba0d2006666be09234620b
ouzyu <murahiro927@gmail.com>
2023-08-23 19:13:11 +0900

    [Finish]chapter_3_4

---
+++ learning/chapter_3_4_input.php
@@ -0,0 +1,10 @@
+<?php require '../header.php'; ?>
+
+<form action="chapter_3_4_output.php" method="post">
+    単価 <input type="text" name="price"> ￥
+    x
+    個数<input type="text" name="count"> 個数
+    <input type="submit" value="計算">
+</form>
+
+<?php require '../footer.php'; ?>
