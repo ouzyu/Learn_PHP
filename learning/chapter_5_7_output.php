aed69cdcf92a85d88c5a605de236ec3ab70db3d9
ouzyu <murahiro927@gmail.com>
2023-08-29 00:00:55 +0900

    [Finish]chapter_5_7

---
+++ learning/chapter_5_7_output.php
@@ -0,0 +1,14 @@
+<?php require '../header.php'; ?>
+
+<?php $file = 'board.text';
+if (file_exists($file)) {
+    $board = json_decode(file_get_contents($file));
+}
+$board[] = htmlspecialchars($_REQUEST['message']);
+file_put_contents($file, json_encode($board));
+foreach ($board as $message) {
+    echo '<p>', $message, '</?><hr>';
+}
+?>
+
+<?php require '../footer.php'; ?>
