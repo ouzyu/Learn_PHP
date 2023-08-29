3f4784a8862a1d165c6dee678723ff3288515771
ouzyu <murahiro927@gmail.com>
2023-08-29 18:46:24 +0900

    [Finish]chapter_6_7

---
+++ learning/chapter6/chapter_6_7_input.php
@@ -0,0 +1,26 @@
+<?php require '../../header.php'; ?>
+
+<table>
+    <tr>
+        <th>商品番号</th>
+        <th>商品名</th>
+        <th>価格</th>
+        <th></th>
+    </tr>
+    <?php
+    $pdo = new PDO('mysql:host=localhost; dbname=shop; charset=utf8', 'staff', 'password');
+    foreach ($pdo->query('select * from product') as $row) {
+        echo '<tr>';
+        echo '<td>', htmlspecialchars($row['id']), '</td>';
+        echo '<td>', htmlspecialchars($row['name']), '</td>';
+        echo '<td>', htmlspecialchars($row['price']), '</td>';
+        echo '<td>';
+        echo '<a href="chapter_6_7_output.php?id=', $row['id'], '">削除</a>';
+        echo '</td>';
+        echo '</tr>';
+        echo "\n";
+    }
+    ?>
+</table>
+
+<?php require '../../footer.php'; ?>
